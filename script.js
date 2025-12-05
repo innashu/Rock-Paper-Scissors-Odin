9d6348a52e213d6b244eb01c4b39e2931d7ad237
206150865+innashu@users.noreply.github.com <innashu@users.noreply.github.comgit pushclear>
2025-12-05 12:01:24 -0800

    Updated Computer Commands

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+    function getComputerChoice() {
+        let number = parseInt(Math.random() * 3);
+
+        if (number === 0 ) {
+            return "Rock";
+        } else if (number === 1) {
+            return "Paper"
+        } else {
+            return "Scissors";
+        }
+
+    }
+        console.log(getComputerChoice());
+
+
+
+
+
