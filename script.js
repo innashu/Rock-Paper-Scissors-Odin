63797c2b7f2384353da5aefd571443a630db9172
206150865+innashu@users.noreply.github.com <innashu@users.noreply.github.comgit pushclear>
2025-12-05 15:43:50 -0800

    Initial Logic for Entire Game

--- script.js
+++ script.js
@@ -23,47 +23,60 @@
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
 
-    let humanScore = 0;
-    let computerScore = 0;
+    function playGame() {
 
-    function playRound(humanChoice, computerChoice) {
-    
-        if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-            return "You Win! Rock Beats Scissors";
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore++;
-            return "You Lose! Paper Beats Rock";
-        } else if (humanChoice === "rock" && computerChoice === "rock") {
-            return "It's a Draw!";
+        let humanScore = 0;
+        let computerScore = 0;
 
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++;
-            return "You Lose! Scissors Beats Paper!";
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            return "You Win! Paper Beats Rock!";
-        } else if (humanChoice === "paper" && computerChoice === "paper") {
-            return "It's a Draw!";
+        function playRound(humanChoice, computerChoice) {
+        
+            if (humanChoice === "rock" && computerChoice === "scissors") {
+                humanScore++;
+                return "You Win! Rock Beats Scissors";
+            } else if (humanChoice === "rock" && computerChoice === "paper") {
+                computerScore++;
+                return "You Lose! Paper Beats Rock";
+            } else if (humanChoice === "rock" && computerChoice === "rock") {
+                return "It's a Draw!";
 
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            return "You Lose! Rock Beats Scissors!";
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            return "You Win! Scissors Beats Paper!";
-        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-            return "It's a Draw!";
+            } else if (humanChoice === "paper" && computerChoice === "scissors") {
+                computerScore++;
+                return "You Lose! Scissors Beats Paper!";
+            } else if (humanChoice === "paper" && computerChoice === "rock") {
+                humanScore++;
+                return "You Win! Paper Beats Rock!";
+            } else if (humanChoice === "paper" && computerChoice === "paper") {
+                return "It's a Draw!";
+
+            } else if (humanChoice === "scissors" && computerChoice === "rock") {
+                return "You Lose! Rock Beats Scissors!";
+            } else if (humanChoice === "scissors" && computerChoice === "paper") {
+                return "You Win! Scissors Beats Paper!";
+            } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+                return "It's a Draw!";
+            }
+
         }
 
+            for (i = 0; i < 5; i++) {
+                
+            }
+
+        }
+
+        const human = getHumanChoice()
+        const computer = getComputerChoice();
+
+        console.log(capitalize(human));
+        console.log(capitalize(computer));
+        
+        const result = playRound(human, computer);
+
+        console.log(result);
+        console.log(humanScore)
+        console.log(computerScore);
+
+    
     }
 
-    const human = getHumanChoice()
-    const computer = getComputerChoice();
-
-    console.log(capitalize(human));
-    console.log(capitalize(computer));
     
-    const result = playRound(human, computer);
-
-    console.log(result);
-    console.log(humanScore)
-    console.log(computerScore);
